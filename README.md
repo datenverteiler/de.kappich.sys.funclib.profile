8efbc566976209aaae9662ab0556ae479afe8502
Falko Schumann <falko.schumann@muspellheim.de>
2016-02-27 20:01:29 +0100

    Projekt mit dem Code von Kernsoftware v3.6.5 initialisiert

---
+++ README.md
@@ -0,0 +1,11 @@
+de.kappich.sys.funclib.profile v${project.version} [![Build Status](https://travis-ci.org/datenverteiler/de.kappich.sys.funclib.profile.svg?branch=master)](https://travis-ci.org/datenverteiler/de.kappich.sys.funclib.profile)
+=====================================
+
+
+Bauen und Veröffentlichen
+-------------------------
+
+    install site assembly:single
+
+Im Ordner `target` wird damit das Distributionspaket
+`SWE_{Name}_{Status}_V{Version}_D{Standdatum}.zip` erstellt.
---
+++ src/main/java/de/kappich/sys/funclib/profile/PackageRuntimeInfo.java
@@ -0,0 +1,14 @@
+
+            package de.kappich.sys.funclib.profile;
+            public class PackageRuntimeInfo {
+            public static String getLicence() { return "GPL";}
+            public static String getRelease() { return "Kernsoftware 3.6.5";}
+            public static String getVersion() { return "3.6.5";}
+            public static String getRevision() { return "13522";}
+            public static String getCompileTime() { return "21.05.2015 19:18:19";}
+            public static String getDependsOnCompiled() { return "";}
+            public static String getDependsOnSource() { return "";}
+            public static String getDependsOnLib() { return "";}
+            public static String getJvmVersion() { return "1.6";}
+            }
+        
